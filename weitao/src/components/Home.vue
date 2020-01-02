7b8f7842a03f7104c831bf45a18ac12e115cc599
wsb260 <260669142@qq.com>
2020-01-02 22:58:48 +0800

    主页布局

--- weitao/src/components/Home.vue
+++ weitao/src/components/Home.vue
@@ -1,7 +1,15 @@
 <template>
-    <div>
-        <el-button type="info" @click="logout">退出</el-button>
-    </div>
+    <el-container class="home-container">
+        <!-- 头部区域 -->
+        <el-header>Header<el-button type="info" @click="logout">退出</el-button></el-header>
+        <!-- 页面主体区域 -->
+        <el-container>
+            <!-- 侧边栏 -->
+            <el-aside width="200px">Aside</el-aside>
+            <!-- 右侧内容主体 -->
+            <el-main>main</el-main>
+        </el-container>        
+    </el-container>
 </template>
 
 <script>
@@ -18,6 +26,18 @@ export default {
 }
 </script>
 
-<style lang="sass">
-    
+<style lang="less">
+.home-container{
+    width:100%;
+    height:100%;
+}
+.el-header{
+    background: #373d41
+}
+.el-aside{
+    background: #333744
+}
+.el-main{
+    background: #EAEDF1
+}
 </style>
